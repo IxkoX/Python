42736680b85c09f4baa54b6355abf718883155c6
IxkoX <frantisek.dedek@gmail.com>
2024-11-19 18:28:38 +0100

    19.11.

---
+++ Lekce_03/users copy.py
@@ -0,0 +1,54 @@
+# user.py
+
+import os, json
+
+
+DATA_PATH = 'Lekce_03/users.json'
+
+def read_data():
+    with open(DATA_PATH, encoding='utf-8') as file:
+        return json.load(file)
+    
+def write_data(data):
+    with open(DATA_PATH, mode="w", encoding='utf-8') as file:
+        json.dump(data, file) #zapiš to do json
+
+def check_password(password, password_repeat):
+    if password != password_repeat:
+        raise ValueError('Hesla se neshodují')
+    
+def check_username(data, username):
+    if username in data:
+        raise ValueError('Uživatel již existuje')
+
+def register(username, password, password_repeat):
+    check_password(password, password_repeat)
+    data = read_data()
+    check_username(username, data) 
+    data[username] = password
+    data = write_data(data)
+
+def login(username, password):
+    data = read_data()
+    try: 
+        assert data[username] == password, 'Chybné heslo'
+        return True
+    except (KeyError, AssertionError):
+        return False
+
+def logout(username):
+    pass
+
+def change_password(old_password, password, password_repeat):
+    pass
+
+def delete_user(username, password):
+    data = read_data()
+    if username in data and data[username] == password:
+        del data[username]
+        write_data(data)
+
+
+#register('test4', 'heslo', 'heslo')
+print(login('tes', 'heslo'))
+delete_user('test4','heslo')
---
+++ Lekce_03/users.py
@@ -0,0 +1,51 @@
+# user.py
+
+import os, json
+
+
+DATA_PATH = 'Lekce_03/users.json'
+
+def read_data():
+    with open(DATA_PATH, encoding='utf-8') as file:
+        return json.load(file)
+    
+def write_data(data):
+    with open(DATA_PATH, mode="w", encoding='utf-8') as file:
+        json.dump(data, file) #zapiš to do json
+
+def register(username, password, password_repeat):
+    if password != password_repeat:
+        raise ValueError('Hesla se neshodují')
+    
+    # Takhle neotvírat soubory ke čtení
+    # file = open(DATA_PATH, encoding='utf-8')
+    # print(file.read())
+    # file.close()
+    
+    # Ale takto ano - pomocí with - což je kontextový manažer
+    
+    data = read_data()
+
+    if username in data:
+        raise ValueError('Uživatel již existuje')
+        
+    data[username] = password
+    data = write_data(data)
+
+    # Toto jsme ud2lali univerzalni do samostatne funkce
+    # with open(DATA_PATH, mode="w", encoding='utf-8') as file:
+    #     json.dump(data, file) #zapiš to do json
+    
+
+def login(username, password):
+    pass
+
+def logout(username):
+    pass
+
+def change_password(old_password, password, password_repeat):
+    pass
+
+
+register('test', 'heslo', 'heslo')
+
